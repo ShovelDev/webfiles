e9a3f812fe0864492cdd881b75953534891a02a1
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2021-12-19 15:59:52 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function hidepopup(popup){
-	document.querySelector(popup).style.display="none"
-}
-
-function search(){
-	var search = document.getElementById('search').value
-	window.open("websites/" + search + "/")
-}
