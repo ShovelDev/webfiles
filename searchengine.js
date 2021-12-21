d7516139c9ec218a1746ff567e34cab98e658b5e
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2021-12-21 13:04:12 -0300

    Add files via upload

--- searchengine.js
+++ searchengine.js
@@ -1,14 +1,16 @@
 var navbar = 0
 
 function search(url){
-	if (document.getElementById('search').value != "") {
+	if (document.getElementById('search').value != "" || document.getElementById('search1').value != "") {
 		var link = document.location.href.replace("index.html", "")
 		finallink = link + "/search/" + url + "/"
 
 		var histname = localStorage.length
 		localStorage.setItem("Site Search (" + histname + ") ", url)
 
-		document.location.href = finallink
+		localStorage.setItem("search", url)
+
+		document.location.href = document.location.href.replace("index.html", "") + "results.html"
 	}
 }
 
@@ -18,24 +20,6 @@ function saveHist(url="index.html"){
 
 	var histname = localStorage.length
 	localStorage.setItem("Site (" + histname + ") ", url)
-}
-
-function reloadResults(){
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('search');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("results");
-  li = ul.getElementsByTagName('li');
-
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
 }
 
 function imlucky(){
