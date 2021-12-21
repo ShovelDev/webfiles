f8f9960f51e0b96c5aa733ad6c9920b31469b952
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2021-12-21 13:04:00 -0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,21 @@
+function reloadResults(){
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('search1');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("results");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+window.onload = function(){
+  reloadResults()
+}
