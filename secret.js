881142d4699c57c69be7d8b4d09725df69c4ca54
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2022-01-02 20:09:49 -0300

    Update secret.js

--- secret.js
+++ secret.js
@@ -9,7 +9,7 @@ if(ester == 0){
 }
 if(ester == 1){
 	document.getElementById("secret").src = "icons/pizza.png"
-	f = `document.querySelector("html").style.background = "yellow";document.querySelector("body").style.background = "yellow";document.body.style.color = "white"`
+	f = `document.querySelector("html").style.background = "yellow";document.querySelector("body").style.background = "yellow";document.body.style.color = "white";document.getElementById("logo1").src="icons/pizza.png"`
 }
 if(ester == 2){
 	document.getElementById("secret").src = "icons/guy.png"
