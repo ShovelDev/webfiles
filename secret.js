c28d520e8d4e20b91a037f934d22ff1f6154a737
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2022-01-02 19:17:44 -0300

    Create secret.js

---
+++ secret.js
@@ -0,0 +1,21 @@
+var ester = parseInt(Math.random() * 2)
+var f = ""
+ester = parseInt(ester)
+
+if(ester == 0){
+	document.getElementById("secret").src = "icons/gamepad.png"
+	f = 'alert("I Like Games 🎮❤")'
+}
+if(ester == 1){
+	document.getElementById("secret").src = "icons/pizza.png"
+	f = `document.body.style.background = "yellow"
+	document.body.style.color = "white`
+}
+if(ester == 2){
+	document.getElementById("secret").src = "icons/guy.png"
+	f = "window.location.href='https://peytta.netlify.app/search/pighit.com/'"
+}
+
+function use(){
+	return f
+}
