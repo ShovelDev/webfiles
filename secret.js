2b3ccbb591955f563c99e508f347494c809d9bfe
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2022-01-02 19:48:00 -0300

    Update secret.js

--- secret.js
+++ secret.js
@@ -20,11 +20,14 @@ function use(){
 	if(!magic){
 		eval(f)
 	}else{
+		document.getElementById("search1").style.transition = "2s";
+		document.getElementById("search1").style.position = "absolute";
+		document.getElementById("search1").style.top = "150%";
+		document.getElementById("search1").style.transform = "rotate(360deg)";
 		
-		
-		document.getElementById("logo1").style.transition = "2s";
-		document.getElementById("logo1").style.position = "absolute";
-		document.getElementById("logo1").style.top = "150%";
-		document.getElementById("logo1").style.transform = "rotate(360deg)";
+		document.getElementById("rs").style.transition = "2s";
+		document.getElementById("rs").style.position = "absolute";
+		document.getElementById("rs").style.top = "150%";
+		document.getElementById("rs").style.transform = "rotate(360deg)";
 	}
 }
