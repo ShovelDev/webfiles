3ca8695f69206962e439b6901c0fd8f32e1fa7bc
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2022-01-02 19:10:27 -0300

    Update searchengine.js

--- searchengine.js
+++ searchengine.js
@@ -1,26 +1,4 @@
 var navbar = 0
-var secret = document.getElementById("secret")
-var ester = Math.random() * 2
-var f = "
-
-if(ester == 0){
-	secret.src = "icons/gamepad.png"
-	f = 'alert("I Like Games 🎮❤")'
-}
-if(ester == 1){
-	secret.src = "icons/pizza.png"
-	f = `document.body.style.background = "yellow"
-	document.body.style.color = "white`
-}
-if(ester == 2){
-	secret.src = "icons/guy.png"
-	f = "window.location.href='https://peytta.netlify.app/search/pighit.com/'"
-}
-
-function use(){
-	return f
-}
-
 function search(url){
 	if (document.getElementById('search').value != "" || document.getElementById('search1').value != "") {
 		var link = document.location.href.replace("index.html", "")
