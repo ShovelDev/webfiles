0ab287b59e7f8193cca4e8d510ac8ed2fc6c71d7
ShovelDev <92690391+ShovelDev@users.noreply.github.com>
2022-01-02 19:52:17 -0300

    Update secret.js

--- secret.js
+++ secret.js
@@ -27,7 +27,7 @@ function use(){
 		
 		document.querySelector("button").style.transition = "2s";
 		document.querySelector("button").style.position = "absolute";
-		document.querySelector("button").style.top = "100%";
+		document.querySelector("button").style.top = "150%";
 		document.getElementById("button").style.transform = "rotate(-360deg)";
 	}
 }
